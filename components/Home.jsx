6c56e88e5c79734f9b272a9efada6d8c499442c1
wahi90 <mdshahed1098@gmail.com>
2022-11-20 12:36:00 +0600

    crate new compenent called home

---
+++ components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './banner';
+import BurgerList from './BurgerList';
+
+const Home = () => {
+    return (
+        <div>
+    <Banner/>
+     <BurgerList/>
+        </div>
+    );
+};
+
+export default Home;
